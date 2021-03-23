2fe6ae652f642489f55e4642df35782fa55d182e
advya <50411324+Advya@users.noreply.github.com>
2021-03-23 11:46:03 +0200

    Lesson 32 started here
    LocalStorage playground

--- public/new.js
+++ public/new.js
@@ -1,6 +1,10 @@
-var x = 1;
+
 
 function something (){
+        var x = window.localStorage.getItem('aaa');
+        window.localStorage.setItem('aaa', 555);
+        
         alert(x);
-        x += 1;
+
+        
  }
