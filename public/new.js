f2a8a2c549b7a42838accdb9901e48b884f8d4d1
advya <50411324+Advya@users.noreply.github.com>
2021-03-23 12:36:16 +0200

    Make x var in LocalStorage

--- public/new.js
+++ public/new.js
@@ -1,13 +1,14 @@
 
 
 function something(){
-        var x = window.localStorage.getItem('aaa');
-                        
-        window.localStorage.setItem('aaa', 555);
+        var x = window.localStorage.getItem('bbb');
+                    
+        x = x*1 + 1;
+        window.localStorage.setItem('bbb', x);
         
         alert(x);        
 }
 
 function more() {
-        alert('more blyat');
+        alert('more bl**t');
 }
