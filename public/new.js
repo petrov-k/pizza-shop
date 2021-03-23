d8e2093aae448765dadf71791eb40cb596288e6b
advya <50411324+Advya@users.noreply.github.com>
2021-03-23 12:26:33 +0200

    Chrome is now working properly
    but Firefox do

--- public/new.js
+++ public/new.js
@@ -1,10 +1,13 @@
 
 
-function something (){
+function something(){
         var x = window.localStorage.getItem('aaa');
+                        
         window.localStorage.setItem('aaa', 555);
         
-        alert(x);
+        alert(x);        
+}
 
-        
- }
+function more() {
+        alert('more blyat');
+}
