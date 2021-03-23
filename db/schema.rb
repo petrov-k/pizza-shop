de6e2882ba7f9f21c535372ef31578ccffa843d6
advya <50411324+Advya@users.noreply.github.com>
2021-03-23 14:13:05 +0200

    bootstrap fixed, site screwed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_23_111432) do
+ActiveRecord::Schema.define(version: 2021_03_23_113936) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
