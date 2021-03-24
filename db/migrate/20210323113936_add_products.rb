3d3d710ab3957395280bc21786b879ae84312f28
advya <50411324+Advya@users.noreply.github.com>
2021-03-24 15:18:57 +0200

    change brackets

--- db/migrate/20210323113936_add_products.rb
+++ db/migrate/20210323113936_add_products.rb
@@ -1,7 +1,7 @@
 class AddProducts < ActiveRecord::Migration[6.1]
   def change
     
-    Product.create :title => "Pizza One", 
+    Product.create ({:title => "Pizza One", 
                    :description => "Beautiful first pizza, have some flavours of ancient relics and spices from our grandfathers.",
                    :price => 12,
                    :size => 30,
@@ -9,7 +9,8 @@ class AddProducts < ActiveRecord::Migration[6.1]
                    :is_veg => false,
                    :is_best_offer => true,
                    :path_to_image => '/images/pizza1.jpg'
-    Product.create :title => "Pizza Two", 
+  })
+    Product.create ({:title => "Pizza Two", 
                    :description => "Beautiful second pizza, have some flavours of ancient relics and spices from our grandfathers.",
                    :price => 13,
                    :size => 35,
@@ -17,7 +18,8 @@ class AddProducts < ActiveRecord::Migration[6.1]
                    :is_veg => false,
                    :is_best_offer => false,
                    :path_to_image => '/images/pizza2.jpg'
-    Product.create :title => "Pizza three", 
+  })
+    Product.create ({:title => "Pizza three", 
                    :description => "Beautiful third pizza, have some flavours of ancient relics and spices from our grandfathers.",
                    :price => 14,
                    :size => 27,
@@ -25,6 +27,6 @@ class AddProducts < ActiveRecord::Migration[6.1]
                    :is_veg => true,
                    :is_best_offer => false,
                    :path_to_image => '/images/pizza3.jpg'
-
+    })
   end
 end
