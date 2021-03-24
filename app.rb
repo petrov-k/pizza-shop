c7ab0b5ddcb976c7aa211a81758dfd14a4263b34
advya <50411324+Advya@users.noreply.github.com>
2021-03-24 16:58:22 +0200

    little fixes

--- app.rb
+++ app.rb
@@ -10,18 +10,15 @@ class Product < ActiveRecord::Base
 
 end
 
-before do 
-	@products = Product.all
-end
-
-
 get '/' do	
+	@products = Product.all
 	erb :index			
 end
 
 get'/about' do
 	erb :about
 end
+
 get '/something' do
 	erb :something
   end
--- public/new.js
+++ public/new.js
@@ -3,7 +3,7 @@
 function something(){
         var x = window.localStorage.getItem('bbb');
                     
-        x = x*1 + 1;
+        x = x * 1 + 1;
         window.localStorage.setItem('bbb', x);
         
         alert(x);        
